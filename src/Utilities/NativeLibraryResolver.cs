112be99f3ce6de29193d7c52b0eaa43f857ee90d
ryancheung <ryancheung.go@gmail.com>
2021-09-23 14:28:37 +0800

    Supress the ModuleInitializer warning

--- src/Utilities/NativeLibraryResolver.cs
+++ src/Utilities/NativeLibraryResolver.cs
@@ -151,7 +151,9 @@ namespace Microsoft.Xna.Framework
 			return "";
 		}
 
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
 		[ModuleInitializer]
+#pragma warning restore CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
 		public static void Init()
 		{
 			// Get the platform and architecture
